ff93a7b991fb542d6ab99fe6bf3a167149ff31c3
khalishaariyani <khalishaaiyani22@gmail.com>
2024-05-10 23:19:09 -0700

    Khalishaariyani6Npraktikum

---
+++ resources/views/mhs/index.blade.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <a href="/">Kembali</a>
+    <h1>DATA MAHASISWA</h1>
+    <a href="\mahasiswa\add">TAMBAH DATA</a>
+    <a href="\mahasiswa\cetak" target="_blank">CETAK PDF</a>
+    <table border="1" cellpadding="10px">
+
+        <tr>
+            <td>NO</td>
+            <td>NPM</td>
+            <td>NNAMAO</td>
+            <td>JURUSAN</td>
+            <td>AKSI/TOMBOL</td>
+        </tr>
+
+        @foreach ($mahasiswa as $nomor => $item)
+            <tr>
+                <td>{{$nomor +1}}</td>
+                <td>{{$item->npm}}</td>
+                <td>{{$item->nama}}</td>
+                <td>{{$item->jurusan}}</td>
+            
+             <td>
+                <a href="/mahasiswa/edit/{{$item->id}}">EDIT</a>
+                <a href="/mahasiswa/delete/{{$item->id}}">HAPUS</a>
+             </td>
+            </tr>
+        @endforeach
+
+    </table>
+</body>
+<h1></h1>
+</html>
